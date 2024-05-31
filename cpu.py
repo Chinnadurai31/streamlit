db97e4deb1d45dfcf1e39f542a09e6f0d172bdd2
Chinnadurai31 <135804225+Chinnadurai31@users.noreply.github.com>
2024-05-31 11:10:58 +0530

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -1,87 +1,12 @@
 import streamlit as st
-from streamlit_echarts import st_echarts
 from datetime import datetime
-import time
 
-st.title("Current Time Gauge")
+# Get the current date and time
+now = datetime.now()
 
-while True:
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
+# Format the date and time
+current_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
-    hours = now.hour
-    minutes = now.minute
-    seconds = now.second
-
-    option = {
-        "tooltip": {
-            "formatter": "{a} <br/>{b} : {c}%",
-        },
-        "series": [
-            {
-                "name": "Hour",
-                "type": "gauge",
-                "min": 0,
-                "max": 24,
-                "splitNumber": 24,
-                "axisLine": {
-                    "lineStyle": {
-                        "color": [[1, "#FF0000"]],
-                        "width": 15,
-                    }
-                },
-                "pointer": {
-                    "length": "80%",
-                    "width": 5,
-                    "color": "auto"
-                },
-                "detail": {"formatter": "{value}h"},
-                "data": [{"value": hours, "name": "Hour"}]
-            },
-            {
-                "name": "Minute",
-                "type": "gauge",
-                "min": 0,
-                "max": 60,
-                "splitNumber": 12,
-                "axisLine": {
-                    "lineStyle": {
-                        "color": [[1, "#00FF00"]],
-                        "width": 15,
-                    }
-                },
-                "pointer": {
-                    "length": "80%",
-                    "width": 5,
-                    "color": "auto"
-                },
-                "detail": {"formatter": "{value}m"},
-                "data": [{"value": minutes, "name": "Minute"}]
-            },
-            {
-                "name": "Second",
-                "type": "gauge",
-                "min": 0,
-                "max": 60,
-                "splitNumber": 10,
-                "axisLine": {
-                    "lineStyle": {
-                        "color": [[1, "#0000FF"]],
-                        "width": 15,
-                    }
-                },
-                "pointer": {
-                    "length": "80%",
-                    "width": 5,
-                    "color": "auto"
-                },
-                "detail": {"formatter": "{value}s"},
-                "data": [{"value": seconds, "name": "Second"}]
-            }
-        ]
-    }
-
-    st_echarts(option, height="500px")
-
-    # Sleep for a second before updating the time
-    time.sleep(1)
+# Display the current date and time in the Streamlit app
+st.write("Current Date and Time:")
+st.write(current_time)
