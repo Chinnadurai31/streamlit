9fae4f9bf07ad0c8bb86f3bd62c7aa07e8034aba
Chinnadurai31 <135804225+Chinnadurai31@users.noreply.github.com>
2024-05-31 11:05:10 +0530

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -1,3 +1,12 @@
 import streamlit as st
+from datetime import datetime
 
-st.write("Hello, World!")
+# Get the current date and time
+now = datetime.now()
+
+# Format the date and time
+current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+# Display the current date and time in the Streamlit app
+st.write("Current Date and Time:")
+st.write(current_time)
